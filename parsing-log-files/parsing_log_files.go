b8f29e2a955a7376a44f0215b8f0673f91a126b4
Abdelmounim Baouia <abdelmounimbaouia@gmail.com>
2023-09-19 09:37:56 +0100

    one more to go 💪

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -1,21 +1,66 @@
 package parsinglogfiles
 
+import (
+	"regexp"
+	"strings"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+  re, err := regexp.Compile(`^(\[((TRC)|(DBG)|(INF)|(WRN)|(ERR)|(FTL))\])`)
+  if err != nil {
+    panic(err)
+  }
+  return re.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+  re, err := regexp.Compile(`<[~*=-]*>`)
+  if err != nil {
+    panic(err)
+  }
+  return re.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+  var s string;
+  for _, line := range lines {
+    s += line;
+  }
+  re, err := regexp.Compile(`(?i)"\w*password\w*"`)
+  if err != nil {
+    panic(err)
+  }
+  matches := re.FindAllStringIndex(s, -1)
+  if matches == nil {
+    return 0
+  }
+  return len(matches[0])
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+  re, err := regexp.Compile(`end-of-line\d+`)
+  if err != nil {
+    panic(err)
+  }
+  return re.ReplaceAllString(text, "")
+
+
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+  userRe, err := regexp.Compile(`User\s+\w+`)
+  if err != nil {
+    panic(err)
+  }
+  var taggedLines []string
+  for _, line := range lines {
+    res := userRe.FindString(line)
+    if res != "" {
+      user := "[USR] " + strings.Fields(res)[1] + " "
+      taggedLines = append(taggedLines, user + line)
+    } else {
+    taggedLines = append(taggedLines, line)
+    }
+  }
+  return taggedLines
 }
