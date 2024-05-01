8a46eda66139aa42e885a11bf3ee7d8afb6b154f
Abdelmounim Baouia <abdelmounimbaouia@gmail.com>
2024-05-01 14:48:15 +0100

    exercise gigasecond

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,8 +1,4 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package gigasecond exports a function AddGigasecond that adds one gigasecond (1e9 seconds) to a given date
 package gigasecond
 
 // import path for the time package from the standard library
@@ -10,12 +6,8 @@ import (
 	"time"
 )
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond: adds one gigasecond (1e9 seconds) to a given date
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	gigasecond := time.Duration(1e9 * float64(time.Second))
 
 	return t.Add(gigasecond)
